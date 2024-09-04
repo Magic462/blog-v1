8ae6de44040608f6c7e217486dda49dbf47f20b4
magic <magic_8@qq.com>
2024-09-04 09:47:23 +0800

    element配置和自定义主题色

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,36 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      //配置elementPlus采用sass样式配色系统
+      resolvers: [ElementPlusResolver({importStyle:"scss"})],
+    }),
     vue(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css:{
+    preprocessorOptions:{
+      scss:{
+        //自动导入定制化样式文件进行样式覆盖
+        additionalData:`
+        @use"style/element/index.scss" as *;
+        `
+      }
     }
   }
 })
